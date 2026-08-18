7e6d491984d5689b5a997fa9a07c0fd629bb5450
jeff-fireship <jeff-fireship@users.noreply.github.com>
2026-08-19 08:50:00 +1000

    feat(terraform): add outputs for ALB, cluster, db and bucket

---
+++ terraform/outputs.tf
@@ -0,0 +1,29 @@
+output "alb_dns_name" {
+  description = "Public DNS name of the load balancer"
+  value       = aws_lb.app.dns_name
+}
+
+output "ecs_cluster_name" {
+  description = "Name of the ECS cluster"
+  value       = aws_ecs_cluster.main.name
+}
+
+output "ecs_service_name" {
+  description = "Name of the ECS service"
+  value       = aws_ecs_service.app.name
+}
+
+output "db_endpoint" {
+  description = "RDS endpoint (host:port)"
+  value       = aws_db_instance.main.endpoint
+}
+
+output "assets_bucket_name" {
+  description = "Name of the static assets bucket"
+  value       = aws_s3_bucket.assets.bucket
+}
+
+output "vpc_id" {
+  description = "ID of the VPC"
+  value       = aws_vpc.main.id
+}
